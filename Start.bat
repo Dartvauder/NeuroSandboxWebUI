39229a743788eedcbde4fb0fded61ce00d9633ce
Michael <dartvauder007@protonmail.com>
2024-03-15 16:43:52 +0200

    Update Start.bat

--- Start.bat
+++ Start.bat
@@ -1,4 +1,5 @@
 @chcp 65001 > NUL
+
 @echo off
 
 set CURRENT_DIR=%~dp0
@@ -6,12 +7,25 @@ set CURRENT_DIR=%~dp0
 call "%CURRENT_DIR%venv\Scripts\activate.bat"
 
 echo Select a file for launch/Выберите файл для запуска:
+
 echo [English version: 1] appEN.py
+
 echo [Русская версия: 2] appRU.py
+
 echo.
+
 set /p choice=Enter number/Введите число:
 
-if "%choice%"=="1" py "%CURRENT_DIR%appEN.py"
-if "%choice%"=="2" py "%CURRENT_DIR%appRU.py"
+if "%choice%"=="1" (
+    start /b py "%CURRENT_DIR%appEN.py"
+    timeout /t 10 > NUL
+    start http://localhost:7860
+)
+
+if "%choice%"=="2" (
+    start /b py "%CURRENT_DIR%appRU.py"
+    timeout /t 10 > NUL
+    start http://localhost:7860
+)
 
 call "%CURRENT_DIR%venv\Scripts\deactivate.bat"
