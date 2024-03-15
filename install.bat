16ecb93807133a8b9832d00170f0c4816db5bd46
Michael <dartvauder007@protonmail.com>
2024-03-15 14:58:26 +0200

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,1 +1,16 @@
-pip install -r requirements.txt
+@chcp 65001
+@echo off
+
+set CURRENT_DIR=%~dp0
+
+py -m venv "%CURRENT_DIR%venv"
+
+call "%CURRENT_DIR%venv\Scripts\activate.bat"
+
+echo Python virtual environment activated/Виртуальное окружение Python активировано.
+
+pip install -r "%CURRENT_DIR%requirements.txt"
+
+echo Packages from requirements.txt were successfully installed/Пакеты из requirements.txt успешно установлены.
+
+pause
