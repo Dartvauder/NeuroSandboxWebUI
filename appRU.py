c49ecda1b8a19e6f272e420bf31c8e3d0cfcf7ce
Michael <dartvauder007@protonmail.com>
2024-03-16 18:19:09 +0200

    Update appRU.py

--- appRU.py
+++ appRU.py
@@ -21,7 +21,8 @@ def transcribe_audio(audio_file_path):
     return result["text"]
 
 
-def generate_text_and_speech(input_text, input_audio, llm_model_name, avatar_name, enable_tts, speaker_wav, language):
+def generate_text_and_speech(input_text, input_audio, llm_model_name, avatar_name, enable_tts, speaker_wav, language,
+                             chat_dir=None):
     prompt = transcribe_audio(input_audio) if input_audio else input_text
     tokenizer, llm_model = load_model(llm_model_name)
     inputs = tokenizer.encode(prompt, return_tensors="pt")
@@ -37,33 +38,33 @@ def generate_text_and_speech(input_text, input_audio, llm_model_name, avatar_nam
         tts = TTS("xtts_v2").to(device)
         wav = tts.tts(text=text, speaker_wav=f"inputs/audio/voices/{speaker_wav}", language=language)
 
+        if not chat_dir:
+            now = datetime.now()
+            chat_dir = os.path.join('outputs', f"chat_{now.strftime('%Y%m%d_%H%M%S')}")
+            os.makedirs(chat_dir)
+            os.makedirs(os.path.join(chat_dir, 'text'))
+            os.makedirs(os.path.join(chat_dir, 'audio'))
+
         now = datetime.now()
         audio_filename = f"output_{now.strftime('%Y%m%d_%H%M%S')}.wav"
-        audio_path = os.path.join('outputs/audio', audio_filename)
-
-        while os.path.exists(audio_path):
-            now = datetime.now()
-            audio_filename = f"output_{now.strftime('%Y%m%d_%H%M%S')}.wav"
-            audio_path = os.path.join('outputs/audio', audio_filename)
+        audio_path = os.path.join(chat_dir, 'audio', audio_filename)
 
         sf.write(audio_path, wav, 22050)
 
-    os.makedirs("outputs/text", exist_ok=True)
-
-    now = datetime.now()
-    chat_history_filename = f"chat_history_{now.strftime('%Y%m%d_%H%M%S')}.txt"
-    chat_history_path = os.path.join('outputs/text', chat_history_filename)
-
-    while os.path.exists(chat_history_path):
+    if not chat_dir:
         now = datetime.now()
-        chat_history_filename = f"chat_history_{now.strftime('%Y%m%d_%H%M%S')}.txt"
-        chat_history_path = os.path.join('outputs/text', chat_history_filename)
+        chat_dir = os.path.join('outputs', f"chat_{now.strftime('%Y%m%d_%H%M%S')}")
+        os.makedirs(chat_dir)
+        os.makedirs(os.path.join(chat_dir, 'text'))
+        os.makedirs(os.path.join(chat_dir, 'audio'))
 
-    with open(chat_history_path, "w", encoding="utf-8") as f:
+    chat_history_path = os.path.join(chat_dir, 'text', 'chat_history.txt')
+
+    with open(chat_history_path, "a", encoding="utf-8") as f:
         f.write(f"Human: {prompt}\n")
         f.write(f"AI: {text}\n\n")
 
-    return text, avatar_path, audio_path
+    return text, avatar_path, audio_path, chat_dir
 
 
 llm_models_list = [model for model in os.listdir("inputs/text/llm_models") if not model.endswith(".txt")]
@@ -80,11 +81,13 @@ iface = gr.Interface(
         gr.Checkbox(label="Включить TTS", value=False),
         gr.Dropdown(choices=speaker_wavs_list, label="Выберите голос", interactive=True),
         gr.Dropdown(choices=["en", "ru"], label="Выберите язык голоса", interactive=True),
+        gr.State()
     ],
     outputs=[
         gr.Textbox(label="Текстовый ответ от LLM", type="text"),
         gr.Image(type="filepath", label="Аватар"),
-        gr.Audio(label="Аудио ответ от LLM", type="filepath")
+        gr.Audio(label="Аудио ответ от LLM", type="filepath"),
+        gr.State()
     ],
     title="НейроЧатWebUI",
     description="Этот пользовательский интерфейс позволяет вам вводить любой текст или аудио и получать "
