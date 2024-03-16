dc0b5067cccc1f17ed4b41029150798e10bddc84
Michael <dartvauder007@protonmail.com>
2024-03-16 15:50:09 +0200

    Update appEN.py

--- appEN.py
+++ appEN.py
@@ -20,22 +20,30 @@ def transcribe_audio(audio_file_path):
     return result["text"]
 
 
-def generate_text_and_speech(input_text, input_audio, llm_model_name, avatar_name, enable_tts,
-                             speaker_wav, language):
+def generate_text_and_speech(input_text, input_audio, llm_model_name, avatar_name, enable_tts, speaker_wav, language):
     prompt = transcribe_audio(input_audio) if input_audio else input_text
     tokenizer, llm_model = load_model(llm_model_name)
     inputs = tokenizer.encode(prompt, return_tensors="pt")
     outputs = llm_model.generate(inputs, max_length=512, pad_token_id=tokenizer.eos_token_id)
     generated_sequence = outputs[0][inputs.shape[-1]:]
     text = tokenizer.decode(generated_sequence, skip_special_tokens=True)
+
     avatar_path = f"inputs/image/avatars/{avatar_name}" if avatar_name else None
     audio_path = None
+
     if enable_tts:
         device = "cuda" if torch.cuda.is_available() else "cpu"
         tts = TTS("xtts_v2").to(device)
         wav = tts.tts(text=text, speaker_wav=f"inputs/audio/voices/{speaker_wav}", language=language)
         sf.write('outputs/audio/output.wav', wav, 22050)
         audio_path = 'outputs/audio/output.wav'
+
+    os.makedirs("outputs/text", exist_ok=True)
+
+    with open("outputs/text/chat_history.txt", "a", encoding="utf-8") as f:
+        f.write(f"Human: {prompt}\n")
+        f.write(f"AI: {text}\n\n")
+
     return text, avatar_path, audio_path
 
 
