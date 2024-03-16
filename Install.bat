1fb5b81125c145263ca8684dcc59ad140b55cc8d
Michael <dartvauder007@protonmail.com>
2024-03-16 21:00:52 +0200

    Add files via upload

--- Install.bat
+++ Install.bat
@@ -1,16 +1,23 @@
-@chcp 65001 > NUL
 @echo off
+chcp 65001 > NUL
 
 set CURRENT_DIR=%~dp0
-
 py -m venv "%CURRENT_DIR%venv"
-
 call "%CURRENT_DIR%venv\Scripts\activate.bat"
 
-echo The application is installing, please wait/Приложение устанавливается, пожалуйста подождите.
+:choice
+set /p choice="Выберите версию для установки/choose a version for install: 1) requirements.txt 2) requirementsCPU.txt: "
 
-pip install -r "%CURRENT_DIR%requirements.txt"
+if "%choice%"=="1" (
+    echo The application is installing, please wait/Приложение устанавливается, пожалуйста подождите.
+    pip install -r "%CURRENT_DIR%requirements.txt"
+) else if "%choice%"=="2" (
+    echo The application is installing, please wait/Приложение устанавливается, пожалуйста подождите.
+    pip install -r "%CURRENT_DIR%requirementsCPU.txt"
+) else (
+    echo Неверный выбор. Попробуйте еще раз.
+    goto choice
+)
 
 echo The application has been installed successfully. Run start.bat/Приложение успешно установлено. Запустите start.bat.
-
 pause
