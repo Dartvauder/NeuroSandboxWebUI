b1466f8e50b417dbaee22b387147106b730cbc38
Michael <dartvauder007@protonmail.com>
2024-03-21 19:28:31 +0200

    Update appEN.py

--- appEN.py
+++ appEN.py
@@ -10,45 +10,54 @@ import warnings
 import logging
 from diffusers import StableDiffusionPipeline
 from git import Repo
+from llama_cpp import Llama
 
 warnings.filterwarnings("ignore")
 logging.getLogger('transformers').setLevel(logging.ERROR)
 logging.getLogger('TTS').setLevel(logging.ERROR)
+logging.getLogger('diffusers').setLevel(logging.ERROR)
 
 
-def load_model(model_name):
-    if model_name:
-        model_path = f"inputs/text/llm_models/{model_name}"
-        tokenizer = AutoTokenizer.from_pretrained(model_path)
-        device = "cuda" if torch.cuda.is_available() else "cpu"
-        model = AutoModelForCausalLM.from_pretrained(model_path)
-        return tokenizer, model.to(device)
+def load_model(model_name, model_type):
+    if model_type == "transformers":
+        if model_name:
+            model_path = f"inputs/text/llm_models/{model_name}"
+            tokenizer = AutoTokenizer.from_pretrained(model_path)
+            device = "cuda" if torch.cuda.is_available() else "cpu"
+            model = AutoModelForCausalLM.from_pretrained(model_path)
+            return tokenizer, model.to(device)
+    elif model_type == "llama":
+        if model_name:
+            model_path = f"inputs/text/llm_models/{model_name}"
+            model = Llama(model_path)
+            return model, None
     return None, None
 
 
 def transcribe_audio(audio_file_path):
     device = "cuda" if torch.cuda.is_available() else "cpu"
-    whisper_model_path = "inputs/text"
-    whisper_repo_path = os.path.join(whisper_model_path, "whisper-medium")
-    if not os.path.exists(whisper_repo_path):
+    whisper_model_path = "inputs/text/whisper-medium"
+    if not os.path.exists(whisper_model_path):
         os.makedirs(whisper_model_path, exist_ok=True)
-        Repo.clone_from("https://huggingface.co/openai/whisper-medium", whisper_repo_path)
+        Repo.clone_from("https://huggingface.co/openai/whisper-medium", whisper_model_path)
     else:
-        repo = Repo(whisper_repo_path)
+        repo = Repo(whisper_model_path)
         repo.remotes.origin.pull()
 
-    model_file = os.path.join(whisper_repo_path, "pytorch_model.bin")
+    model_file = os.path.join(whisper_model_path, "pytorch_model.bin")
     model = whisper.load_model(model_file, device=device)
     result = model.transcribe(audio_file_path)
     return result["text"]
 
 
-def generate_text_and_speech(input_text, input_audio, llm_model_name, max_tokens, temperature, top_p, top_k,
+def generate_text_and_speech(input_text, input_audio, llm_model_name, llm_model_type, max_tokens, temperature, top_p,
+                             top_k,
                              avatar_name, enable_tts, speaker_wav, language, enable_stable_diffusion,
+                             stable_diffusion_model_name,
                              stable_diffusion_steps, stable_diffusion_cfg, stable_diffusion_width,
                              stable_diffusion_height, stable_diffusion_clip_skip, chat_dir=None):
     prompt = transcribe_audio(input_audio) if input_audio else input_text
-    tokenizer, llm_model = load_model(llm_model_name)
+    tokenizer, llm_model = load_model(llm_model_name, llm_model_type)
     tts_model = None
     whisper_model = None
     stable_diffusion_model = None
@@ -56,46 +65,60 @@ def generate_text_and_speech(input_text, input_audio, llm_model_name, max_tokens
     try:
         if enable_tts:
             device = "cuda" if torch.cuda.is_available() else "cpu"
-            tts_model_path = "inputs/audio"
-            tts_repo_path = os.path.join(tts_model_path, "XTTS-v2")
-
-            if not os.path.exists(tts_repo_path):
+            tts_model_path = "inputs/audio/XTTS-v2"
+            if not os.path.exists(tts_model_path):
                 os.makedirs(tts_model_path, exist_ok=True)
-                Repo.clone_from("https://huggingface.co/coqui/XTTS-v2", tts_repo_path)
+                Repo.clone_from("https://huggingface.co/coqui/XTTS-v2", tts_model_path)
             else:
-                repo = Repo(tts_repo_path)
+                repo = Repo(tts_model_path)
                 repo.remotes.origin.pull()
-
-            tts_model = TTS(model_path="inputs/audio/XTTS-v2", config_path="inputs/audio/XTTS-v2/config.json").to(device)
+            tts_model = TTS(model_path=tts_model_path, config_path=f"{tts_model_path}/config.json").to(device)
 
         if input_audio:
             device = "cuda" if torch.cuda.is_available() else "cpu"
-            whisper_model_path = "inputs/text"
-            whisper_repo_path = os.path.join(whisper_model_path, "whisper-medium")
-            if not os.path.exists(whisper_repo_path):
+            whisper_model_path = "inputs/text/whisper-medium"
+            if not os.path.exists(whisper_model_path):
                 os.makedirs(whisper_model_path, exist_ok=True)
-                Repo.clone_from("https://huggingface.co/openai/whisper-medium", whisper_repo_path)
+                Repo.clone_from("https://huggingface.co/openai/whisper-medium", whisper_model_path)
             else:
-                repo = Repo(whisper_repo_path)
+                repo = Repo(whisper_model_path)
                 repo.remotes.origin.pull()
-            whisper_model = whisper.load_model(os.path.join(whisper_repo_path, "pytorch_model.bin"), device=device)
+            whisper_model = whisper.load_model(os.path.join(whisper_model_path, "pytorch_model.bin"), device=device)
 
         if enable_stable_diffusion:
-            stable_diffusion_model_path = "inputs/image"
-            os.makedirs(stable_diffusion_model_path, exist_ok=True)
-            stable_diffusion_model = StableDiffusionPipeline.from_pretrained("runwayml/stable-diffusion-v1-5",
-                                                                             cache_dir=stable_diffusion_model_path)
+            stable_diffusion_model_path = os.path.join("inputs", "image", "sd_models",
+                                                       f"{stable_diffusion_model_name}.safetensors")
+
+            if os.path.exists(stable_diffusion_model_path):
+                stable_diffusion_model = StableDiffusionPipeline.from_single_file(
+                    stable_diffusion_model_path, use_safetensors=True, device_map="auto"
+                )
+            else:
+                print(f"Stable Diffusion model not found: {stable_diffusion_model_path}")
+                stable_diffusion_model = StableDiffusionPipeline.from_pretrained(
+                    "runwayml/stable-diffusion-v1-5", use_safetensors=True, device_map="auto"
+                )
+
             stable_diffusion_model.to("cuda")
+            stable_diffusion_model.text_encoder.to("cuda")
+            stable_diffusion_model.vae.to("cuda")
+            stable_diffusion_model.unet.to("cuda")
+
+            stable_diffusion_model.safety_checker = None
 
         text = None
         if llm_model:
-            inputs = tokenizer.encode(prompt, return_tensors="pt")
-            device = llm_model.device
-            inputs = inputs.to(device)
-            outputs = llm_model.generate(inputs, max_length=max_tokens, top_p=top_p, top_k=top_k,
-                                         temperature=temperature, pad_token_id=tokenizer.eos_token_id)
-            generated_sequence = outputs[0][inputs.shape[-1]:]
-            text = tokenizer.decode(generated_sequence, skip_special_tokens=True)
+            if llm_model_type == "transformers":
+                inputs = tokenizer.encode(prompt, return_tensors="pt")
+                device = llm_model.device
+                inputs = inputs.to(device)
+                outputs = llm_model.generate(inputs, max_length=max_tokens, top_p=top_p, top_k=top_k,
+                                             temperature=temperature, pad_token_id=tokenizer.eos_token_id)
+                generated_sequence = outputs[0][inputs.shape[-1]:]
+                text = tokenizer.decode(generated_sequence, skip_special_tokens=True)
+            elif llm_model_type == "llama":
+                text = llm_model.generate(prompt, max_tokens=max_tokens, temperature=temperature, top_p=top_p,
+                                          top_k=top_k)
 
         avatar_path = f"inputs/image/avatars/{avatar_name}" if avatar_name else None
         audio_path = None
@@ -108,6 +131,7 @@ def generate_text_and_speech(input_text, input_audio, llm_model_name, max_tokens
                 os.makedirs(os.path.join(chat_dir, 'text'))
                 os.makedirs(os.path.join(chat_dir, 'audio'))
                 os.makedirs(os.path.join(chat_dir, 'image'))
+
             now = datetime.now()
             audio_filename = f"output_{now.strftime('%Y%m%d_%H%M%S')}.wav"
             audio_path = os.path.join(chat_dir, 'audio', audio_filename)
@@ -126,6 +150,7 @@ def generate_text_and_speech(input_text, input_audio, llm_model_name, max_tokens
                 os.makedirs(os.path.join(chat_dir, 'text'))
                 os.makedirs(os.path.join(chat_dir, 'audio'))
                 os.makedirs(os.path.join(chat_dir, 'image'))
+
             now = datetime.now()
             image_filename = f"output_{now.strftime('%Y%m%d_%H%M%S')}.png"
             image_path = os.path.join(chat_dir, 'image', image_filename)
@@ -144,6 +169,7 @@ def generate_text_and_speech(input_text, input_audio, llm_model_name, max_tokens
             f.write(f"Human: {prompt}\n")
             if text:
                 f.write(f"AI: {text}\n\n")
+
     finally:
         if tokenizer is not None:
             del tokenizer
@@ -163,6 +189,9 @@ def generate_text_and_speech(input_text, input_audio, llm_model_name, max_tokens
 llm_models_list = [None] + [model for model in os.listdir("inputs/text/llm_models") if not model.endswith(".txt")]
 avatars_list = [None] + [avatar for avatar in os.listdir("inputs/image/avatars") if not avatar.endswith(".txt")]
 speaker_wavs_list = [None] + [wav for wav in os.listdir("inputs/audio/voices") if not wav.endswith(".txt")]
+stable_diffusion_models_list = [None] + [model.replace(".safetensors", "") for model in os.listdir("inputs/image"
+                                                                                                   "/sd_models") if
+                                         model.endswith(".safetensors") or not model.endswith(".txt")]
 
 iface = gr.Interface(
     fn=generate_text_and_speech,
@@ -170,6 +199,7 @@ iface = gr.Interface(
         gr.Textbox(label="Enter your prompt"),
         gr.Audio(type="filepath", label="Record your prompt"),
         gr.Dropdown(choices=llm_models_list, label="Select Language Model", value=None),
+        gr.Dropdown(choices=["transformers", "llama"], label="Select Model Type", value="transformers"),
         gr.Slider(minimum=1, maximum=2048, value=512, step=1, label="Max Tokens"),
         gr.Slider(minimum=0.0, maximum=1.0, value=0.7, step=0.1, label="Temperature"),
         gr.Slider(minimum=0.0, maximum=1.0, value=0.9, step=0.1, label="Top P"),
@@ -179,6 +209,7 @@ iface = gr.Interface(
         gr.Dropdown(choices=speaker_wavs_list, label="Select Voice", interactive=True),
         gr.Dropdown(choices=["en", "ru"], label="Select Language", interactive=True),
         gr.Checkbox(label="Enable Stable Diffusion", value=False),
+        gr.Dropdown(choices=stable_diffusion_models_list, label="Select Stable Diffusion Model", interactive=True),
         gr.Slider(minimum=1, maximum=100, value=30, step=1, label="Stable Diffusion Steps"),
         gr.Slider(minimum=1.0, maximum=30.0, value=8, step=0.1, label="Stable Diffusion CFG"),
         gr.Slider(minimum=256, maximum=1024, value=512, step=64, label="Stable Diffusion Width"),
