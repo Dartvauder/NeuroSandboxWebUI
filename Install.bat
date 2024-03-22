b9b53e01484ec6d518f72ac26be3374ab51a3e63
Michael <dartvauder007@protonmail.com>
2024-03-22 17:57:29 +0200

    Update Install.bat

--- Install.bat
+++ Install.bat
@@ -9,15 +9,17 @@ call "%CURRENT_DIR%venv\Scripts\activate.bat"
 set /p choice="Выберите версию для установки/choose a version for install: 1) GPU 2) CPU: "
 
 if "%choice%"=="1" (
-    echo GPU version is installing, please wait/GPU версия устанавливается, пожалуйста подождите.
+    cls
+    echo GPU version is installing, please wait/GPU версия устанавливается, пожалуйста подождите:
     pip install -r "%CURRENT_DIR%requirementsGPU.txt"
 ) else if "%choice%"=="2" (
-    echo CPU version is installing, please wait/CPU версия устанавливается, пожалуйста подождите.
+    cls
+    echo CPU version is installing, please wait/CPU версия устанавливается, пожалуйста подождите:
     pip install -r "%CURRENT_DIR%requirementsCPU.txt"
 ) else (
-    echo Wronge choose. Try again/Неверный выбор. Попробуйте еще раз.
+    echo Wronge choose. Try again/Неверный выбор. Попробуйте еще раз
     goto choice
 )
 
-echo The application has been installed successfully. Run start.bat/Приложение успешно установлено. Запустите start.bat.
+echo The application has been installed successfully. Run start.bat/Приложение успешно установлено. Запустите start.bat
 pause
