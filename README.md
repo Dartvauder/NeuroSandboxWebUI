8a0b3a7e6315a0fc8c096722728885c7a41c50b6
Michael <dartvauder007@protonmail.com>
2024-03-22 16:52:52 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
-## [Features](/#Features) | [Dependencies](/#Required-Dependencies) | [Install](/How-to-install) | [Usage](/#How-to-use) | [Models](/#Where-can-I-get-models-voices-and-avatars) | [Roadmap](/#Roadmap) | [Acknowledgment](/#Acknowledgment-to-developers)
+## [Features](/#Features) | [Dependencies](/#Required-Dependencies) | [SystemRequirements](/#Minimum-System-Requirements) | [Install](/#How-to-install) | [Usage](/#How-to-use) | [Models](/#Where-can-I-get-models-voices-and-avatars) | [Roadmap](/#Roadmap) | [Acknowledgment](/#Acknowledgment-to-developers)
 
 # ![icon (1)](https://github.com/Dartvauder/NeuroChatWebUI/assets/140557322/e3c1d95a-828f-4a65-bea6-64c336dbe6fa) NeuroChatWebUI (ALPHA)
 * Work in progress!
 * English | [Русский](/README_RU.md)
 ## Description:
 
-Simple and easy interface for chatting with LLM using text or voice input. TTS functions are available here for voice output with a choice of language and voice sample. The goal of the project - to create the easiest possible application for beginners in the topic of neural networks.
+Simple and easy interface for chatting with LLM using text or voice input and also Stable Diffusion for generating images. TTS and Whisper functions are available here for voice input or output with a choice of language and voice sample. The goal of the project - to create the easiest possible application for beginners in the topic of neural networks.
 
 |![Image1](https://github.com/Dartvauder/NeuroChatWebUI/assets/140557322/98e185c1-6011-434c-af9f-885d1c5ded48) |
 |:---:|
@@ -14,9 +14,11 @@ Simple and easy interface for chatting with LLM using text or voice input. TTS f
 
 * Easy install (Windows only)
 * Flexible and optimized interface
-* TTS and STT models
-* Avatar choose (for LLM)
-* Transformers (Llama.cpp in feature)
+* TTS and STT models (For LLM)
+* Avatar choose (For LLM)
+* Transformers and llama.cpp (For LLM)
+* Diffusers and safetensors (For Stable Diffusion)
+* Models settings in interface
 * And much more...
 
 ## Required Dependencies:
@@ -26,49 +28,72 @@ Simple and easy interface for chatting with LLM using text or voice input. TTS f
 * [CUDA](https://developer.nvidia.com/cuda-downloads) (12.1 only)
 * [FFMPEG](https://ffmpeg.org/download.html)
 
+## Minimum System Requirements:
+
+* System: Windows or Linux
+* GPU: 4GB+ or CPU: 8 core 3.2GHZ
+* RAM: 16GB+
+* Disk space: 30GB+
+* Internet for downloading models and installing
+
 ## How to install:
 
 ### Windows
 
-1) `Git clone` or [download](https://github.com/Dartvauder/NeuroChatWebUI/archive/refs/tags/Alpha.zip) the repository.
+1) `Git clone` or [download](https://github.com/Dartvauder/NeuroChatWebUI/archive/refs/tags/Alpha.zip) the repository
 2) Unzip the archive file to any location
-3) Run the `install.bat` and choose a version for installation.
-4) After installation, run `start.bat`.
-5) Select the file version and wait for the application to launch.
+3) Run the `install.bat` and choose a version for installation
+4) After installation, run `start.bat`
+5) Select the file version and wait for the application to launch
 6) Have fun!
 
 To get update, run `update.bat`
 
 ### Linux
 
-1) `Git clone` or [download](https://github.com/Dartvauder/NeuroChatWebUI/archive/refs/tags/Alpha.zip) the repository.
+1) `Git clone` or [download](https://github.com/Dartvauder/NeuroChatWebUI/archive/refs/tags/Alpha.zip) the repository
 2) Unzip the archive file to any location
-3) In the terminal, run the `pip install -r requirementsGPU.txt` or `pip install -r requirementsCPU.txt` and wait for installation of all dependencies.
-4) After installation, run `py appEN.py` or `py appRU.py`.
-5) Wait for the application to launch.
+3) In the terminal, run the `pip install -r requirementsGPU.txt` or `pip install -r requirementsCPU.txt` and wait for installation of all dependencies
+4) After installation, run `py appEN.py` or `py appRU.py`
+5) Wait for the application to launch
 6) Have fun!
 
 To get update, run `git pull`
 
 ## How to use:
 
-* First download the models by folders. Model paths: LLM = inputs/text/llm_models
-#### Optional: Voice sample = inputs/audio/voices; Avatars = inputs/image/avatars
-* To begin, select your model in the `LLM` drop-down list, enter (or say) your prompt and press `generate` button and receive your text response. If you want, you can choose an `avatar`
-#### Optional: You can `enable TTS` mode and select the `voice` and `language` you need to get an audio response
-* You can also click the `clear` button to reset your selection.
+### LLM:
+
+* First download the models by folder: *inputs/text/llm_models*
+#### Optional: Voice sample = *inputs/audio/voices*; Avatars = *inputs/image/avatars*
+* Select model type (`transformers` or `llamacpp`)
+* Сonfigure the model according to the settings you need
+* To begin, select your model in the `LLM` drop-down list, enter (or say) your prompt and press `generate` button to receive your text and audio response. If you want, you can choose an `avatar`
+#### Optional: You can `enable TTS` mode, select the `voice` and `language` you need to get an audio response
+
+### Stable Diffusion:
+
+#### Before generating the image, disable the `LLM` and `TTS` models, and also clear the audio input
+
+* First download the models by folder: *inputs/image/sd_models*
+* Enter your prompt
+* Then select a model from the drop-down list 
+* Сonfigure the model according to the settings you need
+* After, enable `stable diffusion` and press `generate` button to receive your image
+
+P.S. The voice needs to be pre-processed (22050 kHz, mono sound, WAV), avatar should preferably be `PNG` or `JPG`. When you turn on `TTS`, selecting a `language` and `voice` is required, otherwise there will be errors
+
+#### Chat history and generated images is saved in the *outputs* folder
+#### You can also click the `clear` button to reset your selection.
 #### To close the application, close the terminal
-
-The voice needs to be pre-processed (22050 kHz, mono sound, WAV), avatar should preferably be PNG or JPG, the LLM model must be Transformers. When you turn on TTS, selecting a language and voice is required, otherwise there will be errors.
-
-#### Chat history is saved in the outputs folder
 
 ## Where can I get models, voices and avatars?
 
-* Language models can be taken from [HuggingFace](https://huggingface.co/models)
+* LLM models can be taken from [HuggingFace](https://huggingface.co/models)
+* Stable Diffusion models can be taken from [CivitAI](https://civitai.com/models)
 * You can take voices anywhere. Record yours or take a recording from the Internet. The main thing is that it is pre-processed!
 * It’s the same with avatars as with voices. You can download them on the Internet, generate them using neural networks, or take a photo of yourself. The main thing is to comply with the required file format
-* #### TTS and STT models are downloaded automatically
+* #### `TTS` and `STT` models are downloaded automatically in *inputs* folder. The base `diffusers` model of `Stable Diffusion` is also downloaded there 
 
 ## Roadmap
 
@@ -86,10 +111,13 @@ Thank you very much to these projects for allowing me to create my application:
 * `Soundfile` - https://github.com/bastibe/python-soundfile
 * `accelerate` - https://github.com/huggingface/accelerate
 * `cuda-python` - https://github.com/NVIDIA/cuda-python
+* `GitPython` - https://github.com/gitpython-developers/GitPython
+* `diffusers` - https://github.com/huggingface/diffusers
+* `llama.cpp-python` - https://github.com/abetlen/llama-cpp-python
 
 ## Donation
 
-### If you really liked my project and want to donate, here is options to donate. Thank you very much in advance! 
+### *If you liked my project and want to donate, here is options to donate. Thank you very much in advance!*
 
 * CryptoWallet(BEP-20) - 0x3d86bdb5f50b92d0d7Eb44F1a833acC5e91aAEcA
 
---
+++ appRU.py
@@ -0,0 +1,238 @@
+import gradio as gr
+from transformers import AutoModelForCausalLM, AutoTokenizer
+import soundfile as sf
+import os
+import torch
+from TTS.api import TTS
+import whisper
+from datetime import datetime
+import warnings
+import logging
+from diffusers import StableDiffusionPipeline
+from git import Repo
+from llama_cpp import Llama
+import requests
+
+warnings.filterwarnings("ignore")
+logging.getLogger('transformers').setLevel(logging.ERROR)
+logging.getLogger('TTS').setLevel(logging.ERROR)
+logging.getLogger('diffusers').setLevel(logging.ERROR)
+
+
+def load_model(model_name, model_type):
+    if model_type == "transformers":
+        if model_name:
+            model_path = f"inputs/text/llm_models/{model_name}"
+            tokenizer = AutoTokenizer.from_pretrained(model_path)
+            device = "cuda" if torch.cuda.is_available() else "cpu"
+            model = AutoModelForCausalLM.from_pretrained(model_path)
+            return tokenizer, model.to(device)
+    elif model_type == "llama":
+        if model_name:
+            model_path = f"inputs/text/llm_models/{model_name}"
+            model = Llama(model_path)
+            return model, None
+    return None, None
+
+
+def transcribe_audio(audio_file_path):
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    whisper_model_path = "inputs/text/whisper-medium"
+
+    # Download the medium.pt file from the provided URL
+    if not os.path.exists(whisper_model_path):
+        os.makedirs(whisper_model_path, exist_ok=True)
+        url = ("https://openaipublic.azureedge.net/main/whisper/models"
+               "/345ae4da62f9b3d59415adc60127b97c714f32e89e936602e85993674d08dcb1/medium.pt")
+        r = requests.get(url, allow_redirects=True)
+        open(os.path.join(whisper_model_path, "medium.pt"), "wb").write(r.content)
+
+    model_file = os.path.join(whisper_model_path, "medium.pt")
+    model = whisper.load_model(model_file, device=device)
+    result = model.transcribe(audio_file_path)
+    return result["text"]
+
+
+def generate_text_and_speech(input_text, input_audio, llm_model_name, llm_model_type, max_tokens, temperature, top_p,
+                             top_k, avatar_name, enable_tts, speaker_wav, language, enable_stable_diffusion,
+                             stable_diffusion_model_name, stable_diffusion_steps, stable_diffusion_cfg,
+                             stable_diffusion_width, stable_diffusion_height, stable_diffusion_clip_skip,
+                             chat_dir=None):
+    prompt = transcribe_audio(input_audio) if input_audio else input_text
+
+    tokenizer, llm_model = load_model(llm_model_name, llm_model_type)
+
+    tts_model = None
+    whisper_model = None
+    stable_diffusion_model = None
+
+    try:
+        if enable_tts:
+            device = "cuda" if torch.cuda.is_available() else "cpu"
+            tts_model_path = "inputs/audio/XTTS-v2"
+            if not os.path.exists(tts_model_path):
+                os.makedirs(tts_model_path, exist_ok=True)
+                Repo.clone_from("https://huggingface.co/coqui/XTTS-v2", tts_model_path)
+            else:
+                repo = Repo(tts_model_path)
+                repo.remotes.origin.pull()
+            tts_model = TTS(model_path=tts_model_path, config_path=f"{tts_model_path}/config.json").to(device)
+
+        if input_audio:
+            device = "cuda" if torch.cuda.is_available() else "cpu"
+            whisper_model_path = "inputs/text/whisper-medium"
+
+            # Download the medium.pt file from the provided URL
+            if not os.path.exists(whisper_model_path):
+                os.makedirs(whisper_model_path, exist_ok=True)
+                url = ("https://openaipublic.azureedge.net/main/whisper/models"
+                       "/345ae4da62f9b3d59415adc60127b97c714f32e89e936602e85993674d08dcb1/medium.pt")
+                r = requests.get(url, allow_redirects=True)
+                open(os.path.join(whisper_model_path, "medium.pt"), "wb").write(r.content)
+
+            model_file = os.path.join(whisper_model_path, "medium.pt")
+            whisper_model = whisper.load_model(model_file, device=device)
+
+        if enable_stable_diffusion:
+            stable_diffusion_model_path = os.path.join("inputs", "image", "sd_models",
+                                                       f"{stable_diffusion_model_name}.safetensors")
+            if os.path.exists(stable_diffusion_model_path):
+                stable_diffusion_model = StableDiffusionPipeline.from_single_file(
+                    stable_diffusion_model_path, use_safetensors=True, device_map="auto"
+                )
+            else:
+                print(f"Stable Diffusion model not found: {stable_diffusion_model_path}")
+                stable_diffusion_model = StableDiffusionPipeline.from_pretrained(
+                    "runwayml/stable-diffusion-v1-5", use_safetensors=True, device_map="auto"
+                )
+            stable_diffusion_model.to("cuda")
+            stable_diffusion_model.text_encoder.to("cuda")
+            stable_diffusion_model.vae.to("cuda")
+            stable_diffusion_model.unet.to("cuda")
+            stable_diffusion_model.safety_checker = None
+
+        text = None
+        if llm_model:
+            if llm_model_type == "transformers":
+                inputs = tokenizer.encode(prompt, return_tensors="pt")
+                device = llm_model.device
+                inputs = inputs.to(device)
+                outputs = llm_model.generate(inputs, max_length=max_tokens, top_p=top_p, top_k=top_k,
+                                             temperature=temperature, pad_token_id=tokenizer.eos_token_id)
+                generated_sequence = outputs[0][inputs.shape[-1]:]
+                text = tokenizer.decode(generated_sequence, skip_special_tokens=True)
+            elif llm_model_type == "llama":
+                text = llm_model(prompt, max_tokens=max_tokens, temperature=temperature, top_p=top_p,
+                                 top_k=top_k)
+
+        avatar_path = f"inputs/image/avatars/{avatar_name}" if avatar_name else None
+        audio_path = None
+        if enable_tts and text:
+            wav = tts_model.tts(text=text, speaker_wav=f"inputs/audio/voices/{speaker_wav}", language=language)
+            if not chat_dir:
+                now = datetime.now()
+                chat_dir = os.path.join('outputs', f"chat_{now.strftime('%Y%m%d_%H%M%S')}")
+                os.makedirs(chat_dir)
+                os.makedirs(os.path.join(chat_dir, 'text'))
+                os.makedirs(os.path.join(chat_dir, 'audio'))
+                os.makedirs(os.path.join(chat_dir, 'image'))
+            now = datetime.now()
+            audio_filename = f"output_{now.strftime('%Y%m%d_%H%M%S')}.wav"
+            audio_path = os.path.join(chat_dir, 'audio', audio_filename)
+            sf.write(audio_path, wav, 22050)
+
+        image_path = None
+        if enable_stable_diffusion:
+            images = stable_diffusion_model(prompt, num_inference_steps=stable_diffusion_steps,
+                                            guidance_scale=stable_diffusion_cfg, height=stable_diffusion_height,
+                                            width=stable_diffusion_width, clip_skip=stable_diffusion_clip_skip)
+            image = images["images"][0]
+            if not chat_dir:
+                now = datetime.now()
+                chat_dir = os.path.join('outputs', f"chat_{now.strftime('%Y%m%d_%H%M%S')}")
+                os.makedirs(chat_dir)
+                os.makedirs(os.path.join(chat_dir, 'text'))
+                os.makedirs(os.path.join(chat_dir, 'audio'))
+                os.makedirs(os.path.join(chat_dir, 'image'))
+            now = datetime.now()
+            image_filename = f"output_{now.strftime('%Y%m%d_%H%M%S')}.png"
+            image_path = os.path.join(chat_dir, 'image', image_filename)
+            image.save(image_path, format="PNG")
+
+        if not chat_dir:
+            now = datetime.now()
+            chat_dir = os.path.join('outputs', f"chat_{now.strftime('%Y%m%d_%H%M%S')}")
+            os.makedirs(chat_dir)
+            os.makedirs(os.path.join(chat_dir, 'text'))
+            os.makedirs(os.path.join(chat_dir, 'audio'))
+            os.makedirs(os.path.join(chat_dir, 'image'))
+
+        chat_history_path = os.path.join(chat_dir, 'text', 'chat_history.txt')
+        with open(chat_history_path, "a", encoding="utf-8") as f:
+            f.write(f"Human: {prompt}\n")
+            if text:
+                f.write(f"AI: {text}\n\n")
+
+    finally:
+        if tokenizer is not None:
+            del tokenizer
+        if llm_model is not None:
+            del llm_model
+        if tts_model is not None:
+            del tts_model
+        if whisper_model is not None:
+            del whisper_model
+        if stable_diffusion_model is not None:
+            del stable_diffusion_model
+        torch.cuda.empty_cache()
+
+    return text, avatar_path, audio_path, image_path, chat_dir
+
+
+llm_models_list = [None] + [model for model in os.listdir("inputs/text/llm_models") if not model.endswith(".txt")]
+avatars_list = [None] + [avatar for avatar in os.listdir("inputs/image/avatars") if not avatar.endswith(".txt")]
+speaker_wavs_list = [None] + [wav for wav in os.listdir("inputs/audio/voices") if not wav.endswith(".txt")]
+stable_diffusion_models_list = [None] + [model.replace(".safetensors", "") for model in os.listdir("inputs/image"
+                                                                                                   "/sd_models") if
+                                         model.endswith(".safetensors") or not model.endswith(".txt")]
+
+iface = gr.Interface(
+    fn=generate_text_and_speech,
+    inputs=[
+        gr.Textbox(label="Введите ваш запрос"),
+        gr.Audio(type="filepath", label="Запишите ваш запрос"),
+        gr.Dropdown(choices=llm_models_list, label="Выберите LLM модель", value=None),
+        gr.Dropdown(choices=["transformers", "llama"], label="Выберите тип модели", value="transformers"),
+        gr.Slider(minimum=1, maximum=2048, value=512, step=1, label="Максимум токенов"),
+        gr.Slider(minimum=0.0, maximum=1.0, value=0.7, step=0.1, label="Температура"),
+        gr.Slider(minimum=0.0, maximum=1.0, value=0.9, step=0.1, label="Top P"),
+        gr.Slider(minimum=0, maximum=100, value=30, step=1, label="Top K"),
+        gr.Dropdown(choices=avatars_list, label="Выберите аватар", value=None),
+        gr.Checkbox(label="Включить TTS", value=False),
+        gr.Dropdown(choices=speaker_wavs_list, label="Выберите голос", interactive=True),
+        gr.Dropdown(choices=["en", "ru"], label="Выберите язык голоса", interactive=True),
+        gr.Checkbox(label="Включить Stable Diffusion", value=False),
+        gr.Dropdown(choices=stable_diffusion_models_list, label="Выберите модель Stable Diffusion", interactive=True),
+        gr.Slider(minimum=1, maximum=100, value=30, step=1, label="Шаги"),
+        gr.Slider(minimum=1.0, maximum=30.0, value=8, step=0.1, label="CFG"),
+        gr.Slider(minimum=256, maximum=1024, value=512, step=64, label="Ширина"),
+        gr.Slider(minimum=256, maximum=1024, value=512, step=64, label="Высота"),
+        gr.Slider(minimum=1, maximum=4, value=1, step=1, label="Пропуск клипа"),
+        gr.State()
+    ],
+    outputs=[
+        gr.Textbox(label="Текстовый ответ от LLM", type="text"),
+        gr.Image(type="filepath", label="Аватар"),
+        gr.Audio(label="Аудио ответ от LLM", type="filepath"),
+        gr.Image(type="filepath", label="Сгенерированное изображение"),
+        gr.State()
+    ],
+    title="NeuroChatWebUI (ALPHA)",
+    description="Этот пользовательский интерфейс позволяет вам вводить любой текст или аудио и получать "
+                "сгенерированный ответ или изображение. Вы можете выбрать модель, "
+                "аватар, голос и язык из раскрывающихся списков. А так же вы можете настраивать настройки моделей с "
+                "помощью слайдеров. Попробуйте и посмотрите, что произойдет!",
+    allow_flagging="never"
+)
+
+iface.launch()
