5632b844bae95b281214cf6a38a6f67e6e608ba4
Michael <dartvauder007@protonmail.com>
2024-03-30 13:42:04 +0300

    Update Update.bat

--- Update.bat
+++ Update.bat
@@ -7,4 +7,12 @@ set CURRENT_DIR=%~dp0
 
 call "%CURRENT_DIR%venv\Scripts\activate.bat"
 
+REM Update dependencies
+echo Updating dependencies.../Обновление зависимостей...
+pip install --no-deps -r "%CURRENT_DIR%requirements.txt"
+cls
+
+echo Application has been updated successfully. Run start.bat/Приложение успешно обновлено. Запустите start.bat
+pause
+
 pause
