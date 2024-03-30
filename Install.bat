b58a4106a2bc4b3851ea5066461a169023a1afdd
Michael <dartvauder007@protonmail.com>
2024-03-30 12:36:29 +0300

    Add files via upload

--- Install.bat
+++ Install.bat
@@ -2,28 +2,20 @@
 chcp 65001 > NUL
 
 set CURRENT_DIR=%~dp0
+echo Creating virtual environment.../Создание виртуальной среды
 py -m venv "%CURRENT_DIR%venv"
 call "%CURRENT_DIR%venv\Scripts\activate.bat"
+cls
 
 REM Upgrade pip and setuptools
 echo Upgrading pip and setuptools...
 py -m pip install --upgrade pip setuptools
+cls
 
-:choice
-set /p choice="Выберите версию для установки/choose a version for install: 1) GPU 2) CPU: "
-
-if "%choice%"=="1" (
-    cls
-    echo GPU version is installing, please wait/GPU версия устанавливается, пожалуйста подождите:
-    pip install --no-deps -r "%CURRENT_DIR%requirements.txt"
-) else if "%choice%"=="2" (
-    cls
-    echo CPU version is installing, please wait/CPU версия устанавливается, пожалуйста подождите:
-    pip install -r "%CURRENT_DIR%requirementsCPU.txt"
-) else (
-    echo Wronge choose. Try again/Неверный выбор. Попробуйте еще раз
-    goto choice
-)
+REM Install dependencies
+echo Installing dependencies.../Установка зависимостей...
+pip install --no-deps -r "%CURRENT_DIR%requirements.txt"
+cls
 
 echo The application has been installed successfully. Run start.bat/Приложение успешно установлено. Запустите start.bat
 pause
