b03a87ea83f21b26c4be60a1ef5550730afce8ab
Michael <dartvauder007@protonmail.com>
2024-05-04 10:47:08 +0300

    Update Install.bat

--- Install.bat
+++ Install.bat
@@ -13,6 +13,8 @@ cls
 
 echo Installing dependencies.../Установка зависимостей...
 pip install --no-deps -r "%CURRENT_DIR%requirements.txt"
+pip install --no-deps -r "%CURRENT_DIR%requirements-cuda.txt"
+pip install --no-deps -r "%CURRENT_DIR%requirements-llama-cpp.txt"
 cls
 
 echo Application has been installed successfully. Run start.bat/Приложение успешно установлено. Запустите start.bat
