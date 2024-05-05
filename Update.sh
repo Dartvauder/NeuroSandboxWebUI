f3d823d8e20f459c3853438c35004318968a140c
Michael <dartvauder007@protonmail.com>
2024-05-06 00:17:08 +0300

    Update Update.sh

--- Update.sh
+++ Update.sh
@@ -10,6 +10,7 @@ echo "Updating dependencies.../Обновление зависимостей..."
 pip install --no-deps -r "$CURRENT_DIR/requirements.txt"
 pip install --no-deps -r "$CURRENT_DIR/requirements-cuda.txt"
 pip install --no-deps -r "$CURRENT_DIR/requirements-llama-cpp.txt"
+pip install git+https://github.com/tatsy/torchmcubes.git
 clear
 
 echo "Application has been updated successfully. Run start.sh/Приложение успешно обновлено. Запустите start.sh"
