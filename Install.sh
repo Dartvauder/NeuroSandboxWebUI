b4aec9eca08c73f386381ac56253d0e4e349fea4
Michael <dartvauder007@protonmail.com>
2024-08-08 16:50:34 +0300

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -8,7 +8,8 @@ source "$CURRENT_DIR/venv/bin/activate"
 clear
 
 echo "Upgrading pip, setuptools and whell.../Обновление pip, setuptools и wheel..."
-pip install wheel setuptools pip --upgrade
+python3 -m pip install --upgrade pip
+pip install wheel setuptools
 clear
 
 echo "Installing dependencies.../Установка зависимостей..."
