f0aa038ac8a116e380c990058e00907005d1be8a
Michael <dartvauder007@protonmail.com>
2024-08-08 16:51:16 +0300

    Update Update.bat

--- Update.bat
+++ Update.bat
@@ -8,7 +8,8 @@ set CURRENT_DIR=%~dp0
 call "%CURRENT_DIR%venv\Scripts\activate.bat"
 
 echo Updating dependencies.../Обновление зависимостей...
-pip install wheel setuptools pip --upgrade
+python -m pip install --upgrade pip
+pip install wheel setuptools
 pip install --no-deps -r "%CURRENT_DIR%requirements.txt"
 pip install --no-deps -r "%CURRENT_DIR%requirements-cuda.txt"
 pip install --no-deps -r "%CURRENT_DIR%requirements-llama-cpp.txt"
