cf94bc9a84cf8b127b8041fc5eb425a824e10d01
Michael <dartvauder007@protonmail.com>
2024-08-10 15:00:30 +0300

    Update Update.bat

--- Update.bat
+++ Update.bat
@@ -13,6 +13,7 @@ pip install wheel setuptools
 pip install --no-deps -r "%CURRENT_DIR%requirements.txt"
 pip install --no-deps -r "%CURRENT_DIR%requirements-cuda.txt"
 pip install --no-deps -r "%CURRENT_DIR%requirements-llama-cpp.txt"
+pip install git+https://github.com/vork/PyNanoInstantMeshes.git
 pip install git+https://github.com/openai/CLIP.git
 pip install git+https://github.com/tatsy/torchmcubes.git
 cls
