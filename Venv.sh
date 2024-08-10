bbd27d535180c91d6889e5918d12a97ae8765805
Michael <dartvauder007@protonmail.com>
2024-08-10 20:57:41 +0300

    Update Venv.sh

--- Venv.sh
+++ Venv.sh
@@ -4,7 +4,7 @@ CURRENT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 source "${CURRENT_DIR}/venv/bin/activate"
 
-echo "Virtual environment activated/Виртуальное окружение активировано"
-echo "To deactivate the virtual environment, type 'deactivate'/Чтобы деактивировать виртуальное окружение, наберите 'deactivate'."
+echo "Virtual environment activated"
+echo "To deactivate the virtual environment, type 'deactivate'"
 
 bash
