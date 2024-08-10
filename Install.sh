319e861198375a8c4a3a0674899851fe25fef246
Michael <dartvauder007@protonmail.com>
2024-08-10 15:00:22 +0300

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -16,6 +16,7 @@ echo "Installing dependencies.../Установка зависимостей..."
 pip install --no-deps -r "$CURRENT_DIR/requirements.txt"
 pip install --no-deps -r "$CURRENT_DIR/requirements-cuda.txt"
 pip install --no-deps -r "$CURRENT_DIR/requirements-llama-cpp.txt"
+pip install git+https://github.com/vork/PyNanoInstantMeshes.git
 pip install git+https://github.com/openai/CLIP.git
 pip install git+https://github.com/tatsy/torchmcubes.git
 clear
