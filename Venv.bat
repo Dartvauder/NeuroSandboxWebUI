8e60e203120ad5fcd94dbb885481054f03adf67d
Michael <dartvauder007@protonmail.com>
2024-08-10 20:57:20 +0300

    Update Venv.bat

--- Venv.bat
+++ Venv.bat
@@ -5,7 +5,7 @@ set CURRENT_DIR=%~dp0
 
 call "%CURRENT_DIR%venv\Scripts\activate.bat"
 
-echo Virtual environment activated/Виртуальное окружение активировано
-echo To deactivate the virtual environment, type 'deactivate'/Чтобы деактивировать виртуальное окружение, наберите 'deactivate'
+echo Virtual environment activated
+echo To deactivate the virtual environment, type 'deactivate'
 
 cmd /k
