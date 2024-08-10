70a68a880bebccce7df18b8943cd56fadaf4696d
Michael <dartvauder007@protonmail.com>
2024-08-10 20:55:49 +0300

    Update Update.bat

--- Update.bat
+++ Update.bat
@@ -7,18 +7,18 @@ set CURRENT_DIR=%~dp0
 
 call "%CURRENT_DIR%venv\Scripts\activate.bat"
 
-echo Updating dependencies.../Обновление зависимостей...
+echo Updating dependencies...
 python -m pip install --upgrade pip
 pip install wheel setuptools
-pip install --no-deps -r "%CURRENT_DIR%requirements.txt"
-pip install --no-deps -r "%CURRENT_DIR%requirements-cuda.txt"
-pip install --no-deps -r "%CURRENT_DIR%requirements-llama-cpp.txt"
+pip install --no-deps -r "%CURRENT_DIR%RequirementsFiles\requirements.txt"
+pip install --no-deps -r "%CURRENT_DIR%RequirementsFiles\requirements-cuda.txt"
+pip install --no-deps -r "%CURRENT_DIR%RequirementsFiles\requirements-llama-cpp.txt"
 pip install git+https://github.com/vork/PyNanoInstantMeshes.git
 pip install git+https://github.com/openai/CLIP.git
 pip install git+https://github.com/tatsy/torchmcubes.git
 cls
 
-echo Application has been updated successfully. Run start.bat/Приложение успешно обновлено. Запустите start.bat
+echo Application has been updated successfully. Run start.bat
 
 call "%CURRENT_DIR%venv\Scripts\deactivate.bat"
 
