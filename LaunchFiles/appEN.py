9d51bd2e0ba95276f982a767681956c63c3a537e
Michael <dartvauder007@protonmail.com>
2024-08-30 13:50:33 +0300

    OPTIMIZATIONS2

--- LaunchFiles/appEN.py
+++ LaunchFiles/appEN.py
@@ -1,3 +1,47 @@
+import logging
+import os
+import sys
+import warnings
+from io import StringIO
+
+def setup_logging():
+    class StderrCatcher(StringIO):
+        def write(self, txt):
+            if not txt.strip():
+                return
+            logging.getLogger("stderr").error(txt)
+
+    root_logger = logging.getLogger()
+    root_logger.setLevel(logging.DEBUG)
+
+    file_handler = logging.FileHandler('neurosandbox.log', mode='a')
+    file_handler.setLevel(logging.DEBUG)
+    file_formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    file_handler.setFormatter(file_formatter)
+    root_logger.addHandler(file_handler)
+
+    console_handler = logging.StreamHandler()
+    console_handler.setLevel(logging.CRITICAL)
+    root_logger.addHandler(console_handler)
+
+    sys.stderr = StderrCatcher()
+
+    logging.getLogger('tensorflow').setLevel(logging.DEBUG)
+    logging.getLogger('torch').setLevel(logging.DEBUG)
+
+    warnings.filterwarnings("default")
+    logging.captureWarnings(True)
+
+setup_logging()
+
+os.chdir(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
+os.environ["PYTORCH_ENABLE_MPS_FALLBACK"] = "1"
+warnings.filterwarnings("ignore")
+
+logging.getLogger().handlers[1].setLevel(logging.CRITICAL)
+
 import gradio as gr
 import langdetect
 from transformers import AutoModelForCausalLM, AutoTokenizer, AutoProcessor, BarkModel, pipeline, T5EncoderModel, BitsAndBytesConfig, DPTForDepthEstimation, DPTFeatureExtractor
@@ -10,7 +54,6 @@ from scipy.io import wavfile
 import matplotlib.pyplot as plt
 import librosa
 import librosa.display
-import os
 import gc
 import cv2
 import subprocess
@@ -72,8 +115,6 @@ tts_model = None
 whisper_model = None
 audiocraft_model_path = None
 multiband_diffusion_path = None
-
-os.chdir(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 
 def authenticate(username, password):
