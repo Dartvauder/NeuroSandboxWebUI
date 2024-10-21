059e0c647837a464f068eb6d00d819c8bed2888a
Michael <dartvauder007@protonmail.com>
2024-10-21 21:26:47 +0300

    UPD

--- Update.bat
+++ Update.bat
@@ -27,10 +27,10 @@ pip install --no-deps -r "%CURRENT_DIR%RequirementsFiles\requirements.txt" 2>> %
 pip install --no-deps -r "%CURRENT_DIR%RequirementsFiles\requirements-cuda.txt" 2>> %ERROR_LOG%
 pip install --no-deps -r "%CURRENT_DIR%RequirementsFiles\requirements-llama-cpp.txt" 2>> %ERROR_LOG%
 pip install --no-deps -r "%CURRENT_DIR%RequirementsFiles\requirements-stable-diffusion-cpp.txt" 2>> %ERROR_LOG%
-pip install --no-build-isolation -e git+https://github.com/PanQiWei/AutoGPTQ.git#egg=auto_gptq 2>> %ERROR_LOG%
+pip install --no-build-isolation -e git+https://github.com/PanQiWei/AutoGPTQ.git#egg=auto_gptq@v0.7.1 2>> %ERROR_LOG%
 pip install https://huggingface.co/madbuda/triton-windows-builds/resolve/main/triton-2.1.0-cp310-cp310-win_amd64.whl 2>> %ERROR_LOG%
-pip install --no-build-isolation -e git+https://github.com/casper-hansen/AutoAWQ.git#egg=autoawq 2>> %ERROR_LOG%
-pip install --no-build-isolation -e git+https://github.com/turboderp/exllamav2.git#egg=exllamav2 2>> %ERROR_LOG%
+pip install --no-build-isolation -e git+https://github.com/casper-hansen/AutoAWQ.git#egg=autoawq@v0.2.6 2>> %ERROR_LOG%
+pip install --no-build-isolation -e git+https://github.com/turboderp/exllamav2.git#egg=exllamav2@v0.2.3 2>> %ERROR_LOG%
 pip install git+https://github.com/tencent-ailab/IP-Adapter.git 2>> %ERROR_LOG%
 pip install git+https://github.com/vork/PyNanoInstantMeshes.git 2>> %ERROR_LOG%
 pip install git+https://github.com/openai/CLIP.git 2>> %ERROR_LOG%
